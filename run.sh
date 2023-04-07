786227b94052b03f7c567759030bb5ede6796787
faridhaziyev <farid.haziyev@eteam.io>
2023-04-07 11:26:11 +0400

    some fixes

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ gcloud builds submit --config cloudbuild.yaml
 gcloud ai custom-jobs create \
 --region=us-central1 \
 --display-name=aze-ds-firstjob \
---worker-pool-spec=machine-type=n1-standard-8,replica-count=1,container-image-uri=gcr.io/azedstesting/aze-ds-vertex
+--worker-pool-spec=machine-type=n1-standard-8,replica-count=1,container-image-uri=gcr.io/test-azer-ds/aze-ds-vertex
